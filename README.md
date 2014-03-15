717b2b79282838d1a3546c82c57e9caa6d5e630a
zjelveh <zjelveh@gmail.com>
2014-03-15 14:39:42 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 sleePy
 ======
 
-Automate FitBit sleep detection
+Very rudimentary script to see if I can automate sleep detection with my fitbit.
+
+For a user, makes calls to the fitbit API, and records the last sync time and the number of steps walked. Right now this is recorded in a cvs file. The steps data from the API is a cumulative amount for number of steps walked in a day, so I use the difference since last recorded sync to determine activity If a person is not sleeping but hasn't walked for a period of time (set at 2 hours right now), then the script will still record it as sleeping. Also, if a long time passes between syncs, then the script will probably fail.
+
+The following logic determines when a person slept:
+- If time since last sync is less than some fixed time T1
+- Then find the last time the person was sleeping
+- Starting from that time point, compare the number of steps walked in each success period
+- If the change in steps is zero for more than some fixed time T2, then the person is considered asleep
+
+Any new sleeping periods detected are logged with fitbit.
+
+- requires python-fitbit
+https://github.com/orcasgit/python-fitbit
---
+++ sleePy.py
@@ -0,0 +1,78 @@
+import fitbit
+import os
+import datetime
+import time
+import pandas
+
+# threshold for time since last sync, in seconds
+T1 = 43200
+# threshold for deciding if person is sleeping, in seconds
+T2 = 7200
+
+user_sleep = pandas.read_csv('user_sleep.csv') # load csv file with 4 columns: time, steps, diff, sleep
+
+client_key = 'client_key_here'
+client_pass = 'client_pass_here'
+fb = fitbit.FitbitOauthClient(client_key, client_pass)
+
+def check_auth(fb):
+    request_token = fb.fetch_request_token()
+    token_url = fb.authorize_token_url(request_token)
+    # use browser to go to token_url and get the PIN
+    return fb.fetch_access_token(request_token, 'PIN')
+
+user_key, user_secret = check_auth(fb)
+
+fb_auth = fitbit.Fitbit(client_key, client_pass, user_key=user_key, user_secret=user_secret)
+
+last_sync = fb_auth.get_devices()[0]['lastSyncTime']
+last_sync = time.mktime(time.strptime(last_sync,'%Y-%m-%dT%H:%M:%S.000'))
+last_record = user_sleep['time'][user_sleep.shape[0] - 1]
+
+sleep_added = []
+
+if last_sync > last_record:
+    stats = fb_auth._COLLECTION_RESOURCE('activities')
+    steps = stats['summary']['steps']
+    ctime = time.time()
+    user_sleep = user_sleep.append({'time': ctime, 'steps': steps, 'diff': 0, 'sleep': 0}, ignore_index=True)
+    # Only check sleep if time since last sync is less than T2
+    if last_sync - last_record < T1:
+        user_sleep['diff'] = user_sleep['steps'].diff()
+        # find last sleep
+        temp = user_sleep[user_sleep['sleep']==1]
+        if not temp.shape[0]:
+            # no sleep record yet
+            last_sleep_row = 0
+        else:
+            # find row of last sleep
+            last_sleep_row = temp.index[-1]
+        # find first diff between steps
+        last_steps = user_sleep['steps'][last_sleep_row + 1]
+        last_time = user_sleep['time'][last_sleep_row + 1]
+        for row in user_sleep.itertuples():
+            if row[0] > last_sleep_row + 1:
+                if row[2] - last_steps != 0:
+                    last_sleep_row = row[0]
+                    last_steps = user_sleep['steps'][last_sleep_row]
+                    last_time = user_sleep['time'][last_sleep_row]
+                # sleeping
+                if not row[2] - last_steps and row[1] - last_time > T2:
+                    for r in range(last_sleep_row, row[0] + 1):
+                        user_sleep['sleep'][r] = 1
+                        last_sleep_time = r[1]
+                    checker = 0
+                    for i, sleep in enumerate(sleep_added):
+                        if sleep[0] == row[1]:
+                            checker = 1
+                            sleep_added[i][1] = r[1]
+                    if not checker:
+                        sleep_added.append([row[1], r[1]])
+
+if sleep_added:
+    for sleep in sleep_added:
+        start_time = datetime.datetime.fromtimestamp(sleep[0])
+        duration = int((sleep[1] - sleep[0]) * 1000)
+        fb_auth.log_sleep(start_time, duration)
+
+user_sleep.to_csv('user_sleep.csv', index=False)
